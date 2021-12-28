a6982416b62eafc3d6923a66a4b2e43c71096cd2
Charles Sun <2209339564@qq.com>
2021-12-28 20:01:23 +0800

    Make the texture

--- sourcecode/vga/mapping.v
+++ sourcecode/vga/mapping.v
@@ -23,7 +23,7 @@
 //	Give the location,
 // Get the data in the location,
 // Mapping the location to the texture.
-// return the texture leixing
+// return the texture Type
 module mapping(
 	vgaclk,
 	map_x,
@@ -31,72 +31,100 @@ module mapping(
 	ram,
 	texture
     );
-	 input [3:0]map_x;
-	 input [3:0]map_y;
-	 output reg [3:0]texture;
-	 
-	 
-	 wire data_in_the_ram;
+	input vgaclk;
+	input [3:0]map_x;
+	input [3:0]map_y;
+	input [3:0]ram;
+	output reg [3:0]texture;//buffer
+	initial begin
+		texture = 4'b0000;
+	end
+	// Here, get the data in the RAM
+	// and then update the data in the texture buffer
 	//
-	//
-	//
-	//
-
 endmodule
 
+//
 module Texture(
 	clk,
 	type,
-	x_pos,
-	y_pos,
+	x_pos,// Low 4 bit of the position
+	y_pos,// High 4 bit of the position
 	pixel_data
 	);
 	input clk;
 	input [4:0] type;
 	input [4:0] x_pos;
-	input [5:0] y_pos;
+	input [4:0] y_pos;
 	output reg [11:0] pixel_data;
 	localparam WHITE = 12'b1111_1111_1111;
     localparam BLACK = 12'b0000_0000_0000;
     localparam BLUE  = 12'b1111_0000_0000;
     localparam GREEN = 12'b0000_1111_0000;
     localparam RED   = 12'b0000_0000_1111;
+	localparam PURPLE= 12'b1111_0000_1111;
+	localparam YELLOW= 12'b0000_1111_1111;
 	initial begin
 		pixel_data=BLACK;
 	end
 	always @(posedge clk)begin
 		case(type)
-		4'b0000:
+		// Basement
+		4'b0000:// Ground
+			pixel_data= GREEN;
+		4'b0001:// Food
+		if(x_pos>8&&x_pos<24&&y_pos>8&&y_pos<24)
+			pixel_data= YELLOW;
+		else
+			pixel_data= GREEN;
+		4'b0010:// Body -
+		if(x_pos>8&&x_pos<24)
+			pixel_data= BLUE;
+		else
+			pixel_data= GREEN;
+		4'b0011:// Body |
+		if(y_pos>8&&y_pos<24)
+			pixel_data= BLUE;
+		else
+			pixel_data= GREEN;
+		4'b0100:// Body Left Up
+		if((x_pos<8&&y_pos<8)||(x_pos>24||y_pos>24))
+			pixel_data= BLUE;
+		else
+			pixel_data= GREEN;
+		4'b0101:// Body Right Up
+		if((x_pos<8&&y_pos>24)||(x_pos>24||y_pos<8))
+			pixel_data= BLUE;
+		else
+			pixel_data= GREEN;
+		4'b0110:// Body Left Down
+		if((x_pos>24&&y_pos<8)||(x_pos<8||y_pos>24))
+			pixel_data= BLUE;
+		else
+			pixel_data= GREEN;
+		4'b0111:// Body Right Down
+		if((x_pos>24&&y_pos>24)||(x_pos<8||y_pos<8))
+			pixel_data= BLUE;
+		else
+			pixel_data= GREEN;
+		4'b1000:// Tail Left
+		// if((x_pos+y_pos/4>8)&&(x_pos-y_pos/4<24))
+		// 	pixel_data= BLUE;
+		// else
+			pixel_data= GREEN;
+		4'b1001:// Tail Right
 			pixel_data=BLACK;
-		4'b0001:
-			pixel_data=WHITE;
-		4'b0010:
-			pixel_data=BLUE;
-		4'b0011:
-			pixel_data=GREEN;
-		4'b0100:
-			pixel_data=RED;
-		4'b0101:
+		4'b1010:// Tail Up
 			pixel_data=BLACK;
-		4'b0110:
+		4'b1011:// Tail Down
 			pixel_data=BLACK;
-		4'b0111:
+		4'b1100:// Head Left
 			pixel_data=BLACK;
-		4'b1000:
+		4'b1101:// Head Right
 			pixel_data=BLACK;
-		4'b1001:
+		4'b1110:// Head Up
 			pixel_data=BLACK;
-		4'b1010:
-			pixel_data=BLACK;
-		4'b1011:
-			pixel_data=BLACK;
-		4'b1100:
-			pixel_data=BLACK;
-		4'b1101:
-			pixel_data=BLACK;
-		4'b1110:
-			pixel_data=BLACK;
-		4'b1111:
+		4'b1111:// Head Down
 			pixel_data=BLACK;
 		endcase
 	end
